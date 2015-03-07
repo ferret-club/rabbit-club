9d97987cd30187ef9d3f252f414c6c8afbef7294
tiger4th <tiger4th@gmail.com>
2015-03-08 07:21:17 +0900

    扉への行き来2

---
+++ Assets/Rower.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rower : MonoBehaviour {
+
+    void Start() {
+    }
+
+    void Update() {
+    }
+
+
+    public void OnButtonUp () {
+        Vector3 position = new Vector3(0, 400, 0);
+        var hash = new Hashtable
+            {
+                { "amount", position },
+                { "time", 0.5f },
+                { "easetype", iTween.EaseType.easeOutCubic }
+            };
+        iTween.MoveAdd(gameObject, hash);
+    }
+
+    public void OnButtonDown () {
+        Vector3 position = new Vector3(0, -400, 0);
+        var hash = new Hashtable
+            {
+                { "amount", position },
+                { "time", 0.5f },
+                { "easetype", iTween.EaseType.easeOutCubic }
+            };
+        iTween.MoveAdd(gameObject, hash);
+    }
+}
