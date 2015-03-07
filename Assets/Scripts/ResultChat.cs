d2192ea808d30efd421e6b03b7bd589e12cfdcef
kmuramat <kmuramat@yahoo-corp.jp>
2015-03-08 00:24:17 +0900

    リザルト画面のチャット

---
+++ Assets/Scripts/ResultChat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultChat : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		GameObject chatObject = GameObject.Find ("DontDestroyObject/ScrollRect 2") as GameObject;
+		GameObject chatFieldObject = GameObject.Find ("DontDestroyObject/ChatBackground") as GameObject;	
+		GameObject canvas = GameObject.Find ("Canvas") as GameObject;	
+
+		chatObject.transform.parent = canvas.transform;
+		chatFieldObject.transform.parent = canvas.transform;
+	}
+}
