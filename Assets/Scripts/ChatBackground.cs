a20e0a7a8da47dc868284b0d0f4670ef75073e1d
tiger4th <tiger4th@gmail.com>
2015-03-07 22:35:33 +0900

    チャット最大化

---
+++ Assets/Scripts/ChatBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChatBackground : MonoBehaviour {
+
+    GameObject chatBackground;
+
+    void Start () {
+        chatBackground = GameObject.Find("ScrollRect 2/Content");
+    }
+
+    public void OnButtonMaximize () {
+        Vector3 position = new Vector3(0, 1130, 0);
+        var hash = new Hashtable
+            {
+                { "amount", position },
+                { "time", 0.5f },
+                { "easetype", iTween.EaseType.easeOutCubic }
+            };
+        iTween.MoveAdd(chatBackground, hash);
+    }
+
+    public void OnButtonMinimize () {
+        Vector3 position = new Vector3(0, -1130, 0);
+        var hash = new Hashtable
+            {
+                { "amount", position },
+                { "time", 0.5f },
+                { "easetype", iTween.EaseType.easeOutCubic }
+            };
+        iTween.MoveAdd(chatBackground, hash);
+    }
+}
