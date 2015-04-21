4817e00a4b7a874927d914d6db2c3b07cdb331ad
tanahara <sat@tanahara-no-MacBook-Air.local>
2015-04-21 09:37:05 +0900

    時間が経過した時に終了する処理を追加。成功、失敗時ともに時間を引き渡すようにした。

---
+++ Assets/Scripts/FailText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FailText : MonoBehaviour {
+
+	public void OnCall() {
+        Vector3 position = new Vector3(0, -780, 0);
+        var hash = new Hashtable
+            {
+                { "amount", position },
+                { "time", 1.5f },
+                { "easetype", iTween.EaseType.easeOutBounce }
+            };
+        iTween.MoveAdd(gameObject, hash);
+    }
+}
