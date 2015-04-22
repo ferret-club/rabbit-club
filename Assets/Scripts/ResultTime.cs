de8ad7ed266e93067b91c74695d201e397d990a0
tanahara <sat@tanahara-no-MacBook-Air.local>
2015-04-22 10:45:43 +0900

    タブ修正

--- Assets/Scripts/ResultTime.cs
+++ Assets/Scripts/ResultTime.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 public class ResultTime : MonoBehaviour {
 
-	public float resultTime = 0.0f;
+    public float resultTime = 0.0f;
     private Text secText;
     private Text minText;
 
